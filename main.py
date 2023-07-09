b3a073421a43762f10104e45d84c918b12182b68
osahenry11 <84633397+osahenry11@users.noreply.github.com>
2023-07-09 19:43:02 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+import requests
+from bs4 import BeautifulSoup
+from nltk.tokenize import sent_tokenize
+from nltk.stem import PorterStemmer
+from googlesearch import search
+
+def search_topic(topic):
+    # Perform a Google search to retrieve relevant URLs
+    urls = list(search(topic, num_results=5, lang='en', stop=5))
+
+    for url in urls:
+        try:
+            # Send a GET request to the URL and retrieve the web content
+            response = requests.get(url)
+            response.raise_for_status()
+
+            # Parse the HTML content using BeautifulSoup
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # Extract the main text content from the HTML page
+            paragraphs = soup.find_all('p')
+            content = ' '.join([p.text for p in paragraphs])
+
+            # Tokenize the content into sentences
+            sentences = sent_tokenize(content)
+
+            # Process the sentences to find the most relevant answer
+            answer = process_sentences(sentences, topic)
+            if answer:
+                return answer
+
+        except requests.exceptions.RequestException:
+            continue
+
+    # No relevant answer found
+    return "I'm sorry, I couldn't find any information on that topic."
+
+def process_sentences(sentences, topic):
+    ps = PorterStemmer()
+    stemmed_topic = ps.stem(topic.lower())
+
+    for sentence in sentences:
+        tokens = sentence.lower().split()
+        stemmed_tokens = [ps.stem(token) for token in tokens]
+
+        if stemmed_topic in stemmed_tokens:
+            return sentence
+
+    return None
+
+# Main program loop
+while True:
+    topic = input("Enter a topic (or 'q' to quit): ")
+    if topic.lower() == 'q':
+        break
+
+    result = search_topic(topic)
+    print(result)
